ba320375c974d7f60d764e1c674ba1ca91c5e73f
RaBaTen <rafael.baetscher@students.fhnw.ch>
2026-04-08 10:33:26 +0200

    Add main application class for Ticket System

--- src/main/java/com/example/ticketsystem/TicketsystemApplication.java
+++ src/main/java/com/example/ticketsystem/TicketsystemApplication.java
@@ -1,1 +1,12 @@
+package com.example.ticketsystem;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsystemApplication.class, args);
+    }
+}
